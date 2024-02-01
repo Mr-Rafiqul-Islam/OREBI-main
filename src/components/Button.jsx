ca9ad7f7db89d8ca86924d15767acc148c2e8c7c
mr-rafiqul-islam <rafiqulislamemon05@gmail.com>
2024-02-01 20:07:34 +0600

    making cn()

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import cn from '../lib/cn'
 
-const Button = ({className,text}) => {
+const Button = ({className,text, ...restprops}) => {
   return (
-    <button className={`${className} bg-primary text-white font-dmsans font-bold text-sm`}>{text}</button>
+    <button className={cn("bg-primary text-white font-dmsans font-bold text-sm",className) } {...restprops}>{text}</button>
   )
 }
 
