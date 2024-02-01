12a8673d34303f1f1db97a137974cfb228a70051
mr-rafiqul-islam <rafiqulislamemon05@gmail.com>
2024-02-01 20:08:56 +0600

    commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import cn from '../lib/cn'
 
 const Button = ({className,text, ...restprops}) => {
   return (
-    <button className={cn("bg-primary text-white font-dmsans font-bold text-sm",className) } {...restprops}>{text}</button>
+    <button className={cn("bg-primary text-white font-dmsans font-bold text-sm",className) } {...restprops} >{text}</button>
   )
 }
 
