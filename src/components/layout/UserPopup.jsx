38bd08f8264c7fefd7423340ed75b4c782bf0929
mr-rafiqul-islam <rafiqulislamemon05@gmail.com>
2024-02-09 00:40:58 +0600

    popup done using useState

--- src/components/layout/UserPopup.jsx
+++ src/components/layout/UserPopup.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Button from '../Button'
+import { Link } from 'react-router-dom'
 
 function UserPopup({className}) {
   return (
-    <div className={`w-[200px] h-[100px] absolute ${className}`}>
-      <Button text='My Account' className='w-full h-1/2'/>
-      <Button text='Log Out' className='w-full h-1/2 bg-transparent text-primary font-regular border border-[#F0F0F0]'/>
+    <div className={`w-[200px] h-[100px] absolute top-[165px] right-[160px] ${className}`}>
+      <Link to='my-account'><Button text='My Account' className='w-full h-1/2  bg-white transition-all hover:bg-primary hover:text-white text-primary font-regular border border-[#F0F0F0]'/></Link>
+      <Button text='Log Out' className='w-full h-1/2 bg-white transition-all hover:bg-primary hover:text-white text-primary font-regular border border-[#F0F0F0]'/>
     </div>
   )
 }
